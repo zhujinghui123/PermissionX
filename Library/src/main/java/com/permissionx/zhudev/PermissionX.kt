9ce10ac22e7a752a7e5bc6eb0628a74a74287ec2
zhujinghui <zhujinghui@bytedance.com>
2020-10-30 13:58:31 +0800

    The Library finished.

---
+++ Library/src/main/java/com/permissionx/zhudev/PermissionX.kt
@@ -0,0 +1,24 @@
+package com.permissionx.zhudev
+
+import androidx.fragment.app.FragmentActivity
+
+object PermissionX {
+    private const val TAG = "InvisibleFragment"
+
+    fun request(
+        activity: FragmentActivity,
+        vararg permissions: String,
+        callback: PermissionCallback
+    ) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if(existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
+    }
+}
